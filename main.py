8a1604b132fae9fbc7ad723cc44eece3a761d60e
lars.friese@onlinehome.de <lars.friese@onlinehome.de>
2020-01-27 22:17:13 +0100

    Removed Scraper. Updatet Network.

--- main.py
+++ main.py
@@ -10,24 +10,14 @@ import tensorflow_datasets as tfds
 from tensorflow import feature_column
 from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
-# additional packages
-from twitter_scraper import get_tweets
-
-# collect data for processing #1 (via twitter)
-def twitter_scrapers(hashtag_var):
-
-    tweets = []
-    for tweet in get_tweets(hashtag_var, pages=5):
-        tweets.append(tweet)
-    print(str(len(tweets)) + ' tweets succesfully scraped.')
-
-    return tweets
 
 # neural net #1
 # NUMERICAL FEATURES
-def neural_net_numerical_features(url_to_csv, column_to_predict):
+def neural_net_numerical_features(url_to_csv, column_to_predict, list_of_features):
     dataframe = pd.read_csv(url_to_csv)
     dataframe.head()
+    #replace nans and infinities in dataframe
+    dataframe.replace([np.inf, -np.inf], np.nan).dropna(axis=1)
 
     train, test = train_test_split(dataframe, test_size=0.2)
     train, val = train_test_split(train, test_size=0.2)
@@ -44,25 +34,28 @@ def neural_net_numerical_features(url_to_csv, column_to_predict):
             ds = ds.shuffle(buffer_size=len(dataframe))
         ds = ds.batch(batch_size)
         return ds
-
-    batch_size = 32
+    
+    batch_size = 5 # A small batch sized is used for demonstration purposes
     train_ds = df_to_dataset(train, batch_size=batch_size)
     val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
     test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
-
-    example_batch = next(iter(train_ds))[0]
 
     # A utility method to create a feature column
     # and to transform a batch of data
     feature_columns = []
 
     # choose numeric features
-    for header in ['temperaturemin','temperaturemax','precipitation','mist','fastest2minwindspeed']:
+    for header in list_of_features:
         feature_columns.append(feature_column.numeric_column(header))
     
     # layer with features
     feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
     
+    batch_size = 32
+    train_ds = df_to_dataset(train, batch_size=batch_size)
+    val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
+    test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
+
     # model
     model = tf.keras.Sequential([
         feature_layer,
@@ -79,20 +72,20 @@ def neural_net_numerical_features(url_to_csv, column_to_predict):
     # train the model
     model.fit(train_ds,
               validation_data=val_ds,
-              epochs=8)
+              epochs=5)
     
     # model info after training
     loss, accuracy = model.evaluate(test_ds)
     print("Accuracy", accuracy)
-    
+
     # make predictions for column based on feature columns
     predictions = model.predict(test_ds)
-    for prediction, vars()[column_to_predict] in zip(predictions[:10], list(test_ds)[0][1][:10]):
+    for prediction, fog in zip(predictions[:10], list(test_ds)[0][1][:10]):
         print('Predicted ' + column_to_predict + ': {:.2%}'.format(prediction[0]),
               " | Actual outcome: ",
-              ("1" if bool(vars()[column_to_predict]) else "0"))
+              ("1" if bool(fog) else "0"))
 
 if __name__ == '__main__': 
-    neural_net_numerical_features('datasets/weather_dataset.csv','fog')
+    neural_net_numerical_features('datasets/train_dataset.csv','fog',['other', 'worth', 'label'])
     
     
