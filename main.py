36d23d11ba165b2613b25554dd11d6b8241ca36d
lars.friese@onlinehome.de <lars.friese@onlinehome.de>
2020-02-01 11:52:10 +0100

    Prediction update.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import numpy as np
 import csv, sys
 import pandas as pd
+from datetime import date
 # tensorflow
 import tensorflow as tf
 import tensorflow_hub as hub
@@ -13,6 +14,32 @@ from sklearn.model_selection import train_test_split
 #gui imports
 from tkinter import filedialog
 from tkinter import *
+
+def predict_numerical_features(url_to_csv, column_to_predict, model_filename):
+    model = keras.models.load_model(model_filename)
+    
+    dataframe = pd.read_csv(url_to_csv)
+    #replace nans and infinities in dataframe
+    dataframe.replace([np.inf, -np.inf], np.nan).dropna(axis=1)
+
+    def df_to_dataset(dataframe, shuffle=True, batch_size=32):
+        dataframe = dataframe.copy()
+        labels = dataframe.pop(column_to_predict)
+        ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+        if shuffle:
+            ds = ds.shuffle(buffer_size=len(dataframe))
+        ds = ds.batch(batch_size)
+        return ds
+    
+    batch_size = 32
+    full_ds = df_to_dataset(dataframe, batch_size=batch_size)
+
+    predictions = model.predict(full_ds)
+    prediction_result = ''
+    for prediction, vars()[column_to_predict] in zip(predictions[:10], list(test_ds)[0][1][:10]): #vars()[column_to_predict] converts the string inputet to a variable
+        prediction_result += 'Predicted ' + column_to_predict + ': {:.2%}'.format(prediction[0]) + '\n'
+
+    return prediction_result
 
 # neural net #1
 # NUMERICAL FEATURES
@@ -86,8 +113,11 @@ def neural_net_numerical_features(url_to_csv, column_to_predict, list_of_feature
     prediction_result = ''
     for prediction, vars()[column_to_predict] in zip(predictions[:10], list(test_ds)[0][1][:10]): #vars()[column_to_predict] converts the string inputet to a variable
         prediction_result += 'Predicted ' + column_to_predict + ': {:.2%}'.format(prediction[0]) + ' | Actual outcome: ' + ('1' if bool(vars()[column_to_predict]) else '0') + '\n'
+    
+    model.save(str(date.today()) +'.h5') # save model for prediction use later
+    model_name = str(date.today()) + '.h5'
 
-    return accuracy, prediction_result
+    return accuracy, prediction_result, model_name
 
 # GUI
 class popupWindow(object):
@@ -129,18 +159,69 @@ class popupWindow(object):
             self.l3['state'] = 'normal' 
             self.b['state'] = 'normal' 
 
+class popupWindow_predict(object):
+    def __init__(self,master):
+        top=self.top=Toplevel(master)
+        self.choose_button_predict = Button(top, text='Choose CSV file to predict', command=self.choose_predict_file)
+        self.choose_button_predict.pack()
+        self.choose_model_button_predict = Button(top, text='Choose model for prediction', command=self.choose_model_file)
+        self.choose_model_button_predict.pack()
+        self.l_predict=Label(top,text='Column to predict:', state=DISABLED)
+        self.l_predict.pack()
+        self.e_predict=Entry(top, state=DISABLED)
+        self.e_predict.pack()
+        self.b_predict=Button(top,text='Train',command=self.run_predict, state=DISABLED)
+        self.b_predict.pack()
+        self.label_output_predict=Label(top,text='', state=DISABLED)
+        self.label_output_predict.pack()
+    
+    def choose_predict_file(self):
+        global predict_filename
+        predict_filename = filedialog.askopenfilename(initialdir = '/',title = 'Select file',filetypes = (('csv files','*.csv'),('all files','*.*')))
+        print(predict_filename)
+        if len(str(predict_filename)) > 2:
+            self.label_output_predict['text'] = 'Predict File ready.\n'
+            self.e_predict['state'] = 'normal'
+            self.l_predict['state'] = 'normal'
+            self.b_predict['state'] = 'normal'
+            self.label_output_predict['state'] = 'normal'
+    
+    def entryValue_predict(self):
+        return self.value
+
+    def choose_model_file(self):
+        global model_filename
+        model_filename = filedialog.askopenfilename(initialdir = '/',title = 'Select file',filetypes = (('h5 model files','*.h5'),('all files','*.*')))
+        print(model_filename)
+        
+    def run_predict(self):
+        results = predict_numerical_features(predict_filename, self.entryValue_predict(), model_filename)
+        self.label_output_predict['text'] = 'Predictions:\n{}'.format(results)
+
 class mainWindow(object):
     def __init__(self,master):
         self.master=master
-        self.b=Button(master,text="Numeric Feature Model",command=self.popup, anchor=CENTER)
+        self.b=Button(master,text="Numeric Feature Model Train",command=self.popup, anchor=CENTER)
         self.b.pack()
+        self.b_p=Button(master,text="Numeric Feature Model Predict",command=self.popup_predict, anchor=CENTER)
+        self.b_p.pack()
         self.b2=Button(master,text="Train",command=self.run_network, state=DISABLED, anchor=CENTER)
         self.b2.pack()
+        self.b3=Button(master,text="Clear Output",command=self.clear_output, anchor=SE)
+        self.b3.pack()
+        #self.b4=Button(master,text="Predict on new dataset",command=self.predict_new, anchor=SE)
+        #self.b4.pack()
         self.label_output = Label(master, text='')
         self.label_output.pack()
 
     def popup(self):
         self.w=popupWindow(self.master)
+        self.b['state'] = 'disabled' 
+        self.master.wait_window(self.w.top)
+        self.b['state'] = 'normal'
+
+    def popup_predict(self):
+        self.w=popupWindow_predict(self.master)
         self.b['state'] = 'disabled' 
         self.master.wait_window(self.w.top)
         self.b['state'] = 'normal'
@@ -153,11 +234,15 @@ class mainWindow(object):
 
     def entryValue3(self):
         return self.w.value3
-    
+
     def run_network(self):
-        accuracy, prediction_result = neural_net_numerical_features(root.filename,str(self.entryValue()),[x.strip() for x in self.entryValue2().split(',')], int(self.entryValue3()))
+        accuracy, prediction_result, model_name = neural_net_numerical_features(root.filename,str(self.entryValue()),[x.strip() for x in self.entryValue2().split(',')], int(self.entryValue3()))
         self.label_output['text'] += 'Training done. \nModel Accuracy: {}'.format(accuracy)
         self.label_output['text'] += '\nTest Predictions:\n {}'.format(prediction_result)
+        self.label_output['text'] += '\nModel saved:\n {}'.format(model_name)
+    
+    def clear_output(self):
+        self.label_output['text'] = ''
 
 if __name__ == '__main__': 
     root = Tk()
