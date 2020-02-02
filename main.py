715639e0f171f16c8b655163a395f7f49f66aa83
lars.friese@onlinehome.de <lars.friese@onlinehome.de>
2020-02-02 20:13:58 +0100

    First working version of training/predicting numerical values.

--- main.py
+++ main.py
@@ -1,162 +1,89 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 # basic imports
 import numpy as np
-import csv, sys
+import csv, sys, os
 import pandas as pd
 from datetime import date
-# tensorflow
-import tensorflow as tf
-import tensorflow_hub as hub
-import tensorflow_datasets as tfds
-from tensorflow import feature_column
-from tensorflow.keras import layers
-from sklearn.model_selection import train_test_split
 #gui imports
 from tkinter import filedialog
 from tkinter import *
-
-def predict_numerical_features(url_to_csv, column_to_predict, model_filename):
-    model = keras.models.load_model(model_filename)
-    
-    dataframe = pd.read_csv(url_to_csv)
-    #replace nans and infinities in dataframe
-    dataframe.replace([np.inf, -np.inf], np.nan).dropna(axis=1)
-
-    def df_to_dataset(dataframe, shuffle=True, batch_size=32):
-        dataframe = dataframe.copy()
-        labels = dataframe.pop(column_to_predict)
-        ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
-        if shuffle:
-            ds = ds.shuffle(buffer_size=len(dataframe))
-        ds = ds.batch(batch_size)
-        return ds
-    
-    batch_size = 32
-    full_ds = df_to_dataset(dataframe, batch_size=batch_size)
-
-    predictions = model.predict(full_ds)
-    prediction_result = ''
-    for prediction, vars()[column_to_predict] in zip(predictions[:10], list(test_ds)[0][1][:10]): #vars()[column_to_predict] converts the string inputet to a variable
-        prediction_result += 'Predicted ' + column_to_predict + ': {:.2%}'.format(prediction[0]) + '\n'
-
-    return prediction_result
-
-# neural net #1
-# NUMERICAL FEATURES
-def neural_net_numerical_features(url_to_csv, column_to_predict, list_of_features, epochs_amount):
-    dataframe = pd.read_csv(url_to_csv)
-    dataframe.head()
-    #replace nans and infinities in dataframe
-    dataframe.replace([np.inf, -np.inf], np.nan).dropna(axis=1)
-
-    train, test = train_test_split(dataframe, test_size=0.2)
-    train, val = train_test_split(train, test_size=0.2)
-    print(len(train), 'train examples')
-    print(len(val), 'validation examples')
-    print(len(test), 'test examples')
-
-    # A utility method to create a tf.data dataset from a Pandas Dataframe
-    def df_to_dataset(dataframe, shuffle=True, batch_size=32):
-        dataframe = dataframe.copy()
-        labels = dataframe.pop(column_to_predict)
-        ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
-        if shuffle:
-            ds = ds.shuffle(buffer_size=len(dataframe))
-        ds = ds.batch(batch_size)
-        return ds
-    
-    batch_size = 5 # A small batch sized is used for demonstration purposes
-    train_ds = df_to_dataset(train, batch_size=batch_size)
-    val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
-    test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
-
-    # A utility method to create a feature column
-    # and to transform a batch of data
-    feature_columns = []
-
-    # choose numeric features
-    for header in list_of_features:
-        feature_columns.append(feature_column.numeric_column(header))
-    
-    # layer with features
-    feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
-    
-    batch_size = 32
-    train_ds = df_to_dataset(train, batch_size=batch_size)
-    val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
-    test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
-
-    # model
-    model = tf.keras.Sequential([
-        feature_layer,
-        layers.Dense(128, activation='relu'),
-        layers.Dense(128, activation='relu'),
-        layers.Dense(1, activation='sigmoid')
-    ])
-    
-    # optimize the model
-    model.compile(optimizer='adam',
-                  loss='binary_crossentropy',
-                  metrics=['accuracy'])
-    
-    # train the model
-    model.fit(train_ds,
-              validation_data=val_ds,
-              epochs=epochs_amount)
-    
-    # model info after training
-    loss, accuracy = model.evaluate(test_ds)
-    print("Accuracy", accuracy)
-
-    # make predictions for column based on feature columns
-    predictions = model.predict(test_ds)
-    prediction_result = ''
-    for prediction, vars()[column_to_predict] in zip(predictions[:10], list(test_ds)[0][1][:10]): #vars()[column_to_predict] converts the string inputet to a variable
-        prediction_result += 'Predicted ' + column_to_predict + ': {:.2%}'.format(prediction[0]) + ' | Actual outcome: ' + ('1' if bool(vars()[column_to_predict]) else '0') + '\n'
-    
-    model.save(str(date.today()) +'.h5') # save model for prediction use later
-    model_name = str(date.today()) + '.h5'
-
-    return accuracy, prediction_result, model_name
+#NEURAL NET functions from models.py
+from models import*
 
 # GUI
+
 class popupWindow(object):
     def __init__(self,master):
         top=self.top=Toplevel(master)
         self.choose_button = Button(top, text='Choose CSV file', command=self.choose_file)
         self.choose_button.pack()
-        self.l=Label(top,text="Column to predict:", state=DISABLED)
+        self.l=Label(top,text='Column to predict:', state=DISABLED)
         self.l.pack()
         self.e=Entry(top, state=DISABLED)
         self.e.pack()
-        self.l2=Label(top,text="Numerical columns used to predict in a list: (e.g. column1,column2,etc.)", state=DISABLED)
+        self.l2=Label(top,text='Numerical columns used to predict\n in a list: (e.g. column1,column2,etc.)', state=DISABLED)
         self.l2.pack()
         self.e2=Entry(top, state=DISABLED)
         self.e2.pack()
-        self.l3=Label(top,text="Number of epochs:", state=DISABLED)
+        self.l3=Label(top,text='Number of epochs:', state=DISABLED)
         self.l3.pack()
         self.e3=Entry(top, state=DISABLED)
         self.e3.pack()
-        self.b=Button(top,text='Ok',command=self.cleanup, state=DISABLED)
-        self.b.pack()
         
+        self.l4=Label(top,text='Optimizer:', state=DISABLED)
+        self.l4.pack()
+        options = ['SGD','RMSprop','Adagrad','Adadelta','Adam','Adamax','Nadam']
+        variable = StringVar(top)
+        variable.set(options[4]) # default value
+        self.w = OptionMenu(top, variable, *options)
+        self.w.configure(state="disabled")
+        self.w.pack()
+
+        self.b=Button(top,text='Save',command=self.cleanup, state=DISABLED)
+        self.b.pack(padx=5, side=RIGHT, anchor=S)
+        self.b2=Button(top,text='Train',command=self.run_network, state=DISABLED)
+        self.b2.pack(padx=5, side=RIGHT, anchor=S)
+        self.b3=Button(top,text='Clear Output',command=self.clear_output)
+        self.b3.pack(padx=5, side=LEFT, anchor=S)
+        self.label_output = Label(top, text='')
+        self.label_output.pack()
+
+    def run_network(self):
+        accuracy, prediction_result, model_name = neural_net_numerical_features(root.filename,str(self.entryValue()),[x.strip() for x in self.entryValue2().split(',')], int(self.entryValue3()), optimizer_input)
+        self.label_output['text'] += 'Training done. \nModel Accuracy: {}'.format(accuracy)
+        self.label_output['text'] += '\nTest Predictions:\n {}'.format(prediction_result)
+        self.label_output['text'] += '\nModel saved in folder:\n {}'.format(model_name)
+        
+    def entryValue(self):
+        return self.value
+
+    def entryValue2(self):
+        return self.value2
+
+    def entryValue3(self):
+        return self.value3
+    
+    def clear_output(self):
+        self.label_output['text'] = ''
+
     def cleanup(self):
         self.value=self.e.get()
         self.value2=self.e2.get()
         self.value3=self.e3.get()
-        gui.b2['state'] = 'normal'
-        self.top.destroy()
+        self.b2['state'] = 'normal'
     
     def choose_file(self):
         root.filename = filedialog.askopenfilename(initialdir = '/',title = 'Select file',filetypes = (('csv files','*.csv'),('all files','*.*')))
         if len(str(root.filename)) > 2:
-            gui.label_output['text'] = 'File ready.\n'
+            self.label_output['text'] = 'File ready.\n'
             self.e['state'] = 'normal'
             self.e2['state'] = 'normal'
             self.e3['state'] = 'normal'
             self.l['state'] = 'normal'
             self.l2['state'] = 'normal'
-            self.l3['state'] = 'normal' 
+            self.l3['state'] = 'normal'
+            self.w['state'] = 'normal'
+            self.l4['state'] = 'normal' 
             self.b['state'] = 'normal' 
 
 class popupWindow_predict(object):
@@ -164,7 +91,7 @@ class popupWindow_predict(object):
         top=self.top=Toplevel(master)
         self.choose_button_predict = Button(top, text='Choose CSV file to predict', command=self.choose_predict_file)
         self.choose_button_predict.pack()
-        self.choose_model_button_predict = Button(top, text='Choose model for prediction', command=self.choose_model_file)
+        self.choose_model_button_predict = Button(top, text='Choose model folder for prediction', command=self.choose_model_file)
         self.choose_model_button_predict.pack()
         self.l_predict=Label(top,text='Column to predict:', state=DISABLED)
         self.l_predict.pack()
@@ -174,7 +101,7 @@ class popupWindow_predict(object):
         self.b_predict.pack()
         self.label_output_predict=Label(top,text='', state=DISABLED)
         self.label_output_predict.pack()
-    
+        
     def choose_predict_file(self):
         global predict_filename
         predict_filename = filedialog.askopenfilename(initialdir = '/',title = 'Select file',filetypes = (('csv files','*.csv'),('all files','*.*')))
@@ -187,11 +114,11 @@ class popupWindow_predict(object):
             self.label_output_predict['state'] = 'normal'
     
     def entryValue_predict(self):
-        return self.value
+        return self.e_predict.get()
 
     def choose_model_file(self):
         global model_filename
-        model_filename = filedialog.askopenfilename(initialdir = '/',title = 'Select file',filetypes = (('h5 model files','*.h5'),('all files','*.*')))
+        model_filename = filedialog.askdirectory(initialdir = os.getcwd())
         print(model_filename)
         
     def run_predict(self):
@@ -201,18 +128,19 @@ class popupWindow_predict(object):
 class mainWindow(object):
     def __init__(self,master):
         self.master=master
-        self.b=Button(master,text="Numeric Feature Model Train",command=self.popup, anchor=CENTER)
-        self.b.pack()
-        self.b_p=Button(master,text="Numeric Feature Model Predict",command=self.popup_predict, anchor=CENTER)
-        self.b_p.pack()
-        self.b2=Button(master,text="Train",command=self.run_network, state=DISABLED, anchor=CENTER)
-        self.b2.pack()
-        self.b3=Button(master,text="Clear Output",command=self.clear_output, anchor=SE)
-        self.b3.pack()
-        #self.b4=Button(master,text="Predict on new dataset",command=self.predict_new, anchor=SE)
+        self.label1=Label(master,text='Numeric Models')
+        self.label1.config(font=('Sans-serif', 12))
+        self.label1.pack(pady=5,padx=5)
+        self.b=Button(master,text='Numeric Feature Model Train',command=self.popup, anchor=CENTER)
+        self.b.pack(padx=20)
+        self.label2=Label(master,text='Train a model with a csv file\n and save it in a file for later predictions.')
+        self.label2.pack(pady=5)
+        self.b_p=Button(master,text='Numeric Feature Model Predict',command=self.popup_predict, anchor=CENTER)
+        self.b_p.pack(padx=20)
+        self.label3=Label(master,text='Predict columns in a csv file with a saved model.')
+        self.label3.pack(pady=5)
+        #self.b4=Button(master,text='Predict on new dataset',command=self.predict_new, anchor=SE)
         #self.b4.pack()
-        self.label_output = Label(master, text='')
-        self.label_output.pack()
 
     def popup(self):
         self.w=popupWindow(self.master)
@@ -226,26 +154,7 @@ class mainWindow(object):
         self.master.wait_window(self.w.top)
         self.b['state'] = 'normal'
 
-    def entryValue(self):
-        return self.w.value
-
-    def entryValue2(self):
-        return self.w.value2
-
-    def entryValue3(self):
-        return self.w.value3
-
-    def run_network(self):
-        accuracy, prediction_result, model_name = neural_net_numerical_features(root.filename,str(self.entryValue()),[x.strip() for x in self.entryValue2().split(',')], int(self.entryValue3()))
-        self.label_output['text'] += 'Training done. \nModel Accuracy: {}'.format(accuracy)
-        self.label_output['text'] += '\nTest Predictions:\n {}'.format(prediction_result)
-        self.label_output['text'] += '\nModel saved:\n {}'.format(model_name)
-    
-    def clear_output(self):
-        self.label_output['text'] = ''
-
 if __name__ == '__main__': 
-    root = Tk()
+    root = Tk(className='ml_models')
     gui = mainWindow(root)
-    root.geometry('500x200')
     root.mainloop()
---
+++ models.py
@@ -0,0 +1,122 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+# basic imports
+import numpy as np
+import csv, sys, os
+import pandas as pd
+from datetime import date
+# tensorflow
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+from tensorflow import feature_column
+import tensorflow.keras as keras
+from tensorflow.keras import *
+from sklearn.model_selection import train_test_split
+
+# COLLECTION OF NEURAL NETWORK FUNCTIONS
+
+# neural net #1
+# NUMERICAL FEATURES TRAINING
+def neural_net_numerical_features(url_to_csv, column_to_predict, list_of_features, epochs_amount, optimizer_input):
+    dataframe = pd.read_csv(url_to_csv)
+    dataframe.head()
+    #replace nans and infinities in dataframe
+    dataframe.replace([np.inf, -np.inf], np.nan).dropna(axis=1)
+
+    train, test = train_test_split(dataframe, test_size=0.2)
+    train, val = train_test_split(train, test_size=0.2)
+    print(len(train), 'train examples')
+    print(len(val), 'validation examples')
+    print(len(test), 'test examples')
+
+    # A utility method to create a tf.data dataset from a Pandas Dataframe
+    def df_to_dataset(dataframe, shuffle=True, batch_size=32):
+        dataframe = dataframe.copy()
+        labels = dataframe.pop(column_to_predict)
+        ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+        if shuffle:
+            ds = ds.shuffle(buffer_size=len(dataframe))
+        ds = ds.batch(batch_size)
+        return ds
+    
+    batch_size = 5 # A small batch sized is used for demonstration purposes
+    train_ds = df_to_dataset(train, batch_size=batch_size)
+    val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
+    test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
+
+    # A utility method to create a feature column
+    # and to transform a batch of data
+    feature_columns = []
+
+    # choose numeric features
+    for header in list_of_features:
+        feature_columns.append(feature_column.numeric_column(header))
+    
+    # layer with features
+    feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
+    
+    batch_size = 32
+    train_ds = df_to_dataset(train, batch_size=batch_size)
+    val_ds = df_to_dataset(val, shuffle=False, batch_size=batch_size)
+    test_ds = df_to_dataset(test, shuffle=False, batch_size=batch_size)
+
+    # model
+    model = tf.keras.Sequential([
+        feature_layer,
+        layers.Dense(128, activation='relu'),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(1, activation='sigmoid')
+    ])
+    
+    # optimize the model
+    model.compile(optimizer=optimizer_input,
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+    
+    # train the model
+    model.fit(train_ds,
+              validation_data=val_ds,
+              epochs=epochs_amount)
+    
+    # model info after training
+    loss, accuracy = model.evaluate(test_ds)
+    print("Accuracy", accuracy)
+
+    # make predictions for column based on feature columns
+    predictions = model.predict(test_ds)
+    prediction_result = ''
+    for prediction, vars()[column_to_predict] in zip(predictions[:10], list(test_ds)[0][1][:10]): #vars()[column_to_predict] converts the string inputet to a variable
+        prediction_result += 'Predicted ' + column_to_predict + ': {:.2%}'.format(prediction[0]) + ' | Actual outcome: ' + ('1' if bool(vars()[column_to_predict]) else '0') + '\n'
+    
+    model.save(str(date.today()), '/') # save model for prediction use later
+    model_name = str(date.today())
+
+    return accuracy, prediction_result, model_name
+
+# NUMERICAL FEATURES PREDICTION
+def predict_numerical_features(url_to_csv, column_to_predict, model_filename):
+    print(url_to_csv, column_to_predict, model_filename)
+    model = keras.models.load_model(model_filename)
+    
+    
+    dataframe = pd.read_csv(url_to_csv)
+    #replace nans and infinities in dataframe
+    dataframe.replace([np.inf, -np.inf], np.nan).dropna(axis=1)
+
+    def df_to_dataset(dataframe, batch_size=32):
+        dataframe = dataframe.copy()
+        labels = dataframe.pop(column_to_predict)
+        ds = tf.data.Dataset.from_tensor_slices((dict(dataframe), labels))
+        ds = ds.batch(batch_size)
+        return ds
+    
+    batch_size = 32
+    full_ds = df_to_dataset(dataframe, batch_size=batch_size)
+
+    predictions = model.predict(full_ds)
+    prediction_result = ''
+    for prediction, vars()[column_to_predict] in zip(predictions, list(full_ds)[0][1]): #vars()[column_to_predict] converts the string inputet to a variable
+        outcome = 1 if prediction[0] > 0.5 else 0
+        prediction_result += 'Predicted ' + column_to_predict + ': {:.2%}'.format(prediction[0]) + ' : Predicted Outcome: ' + str(outcome) + '\n'
+
+    return prediction_result
